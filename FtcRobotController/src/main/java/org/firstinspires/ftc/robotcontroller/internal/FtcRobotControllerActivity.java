b9013a84fd24a81170265364e25a17fab140bd9f
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2019-11-05 21:07:18 -0500

    Added RoadRunner and FTC Dashboard as dependencies

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -60,6 +60,7 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.google.blocks.ftcrobotcontroller.ProgrammingWebHandlers;
 import com.google.blocks.ftcrobotcontroller.runtime.BlocksOpMode;
 import com.qualcomm.ftccommon.ClassManagerFactory;
@@ -313,6 +314,11 @@ public class FtcRobotControllerActivity extends Activity
           }
         });
         popupMenu.inflate(R.menu.ftc_robot_controller);
+        /*
+
+            POPULATE FTC DASHBOARD DATA IN MENU
+
+         */
         popupMenu.show();
       }
     });
@@ -380,6 +386,13 @@ public class FtcRobotControllerActivity extends Activity
     if (preferencesHelper.readBoolean(getString(R.string.pref_wifi_automute), false)) {
       initWifiMute(true);
     }
+
+    /*
+
+        CREATE FTC DASHBOARD INSTANCE HERE!
+
+     */
+    FtcDashboard.start();
   }
 
   protected UpdateUI createUpdateUI() {
@@ -459,6 +472,13 @@ public class FtcRobotControllerActivity extends Activity
     if (preferencesHelper != null) preferencesHelper.getSharedPreferences().unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener);
 
     RobotLog.cancelWriteLogcatToDisk();
+
+    /*
+
+        STOP FTC DASHBOARD HERE
+
+     */
+    FtcDashboard.stop();
   }
 
   protected void bindToService() {
@@ -531,6 +551,7 @@ public class FtcRobotControllerActivity extends Activity
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.ftc_robot_controller, menu);
+    FtcDashboard.populateMenu(menu);
     return true;
   }
 
@@ -665,6 +686,13 @@ public class FtcRobotControllerActivity extends Activity
         return service.getRobot().eventLoopManager;
       }
     });
+    /*
+
+
+        CREATE FTC DASHBOARD WEB SERVER
+
+     */
+    FtcDashboard.attachWebServer(service.getWebServer());
   }
 
   private void updateUIAndRequestRobotSetup() {
@@ -704,6 +732,12 @@ public class FtcRobotControllerActivity extends Activity
 
     passReceivedUsbAttachmentsToEventLoop();
     AndroidBoard.showErrorIfUnknownControlHub();
+    /*
+
+        ENABLE OPMODE MANAGEMENT IN FTC DASHBOARD
+
+     */
+    FtcDashboard.attachEventLoop(eventLoop);
   }
 
   protected OpModeRegister createOpModeRegister() {
